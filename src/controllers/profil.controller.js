c65a0716b0c49126510b21935ec58e9bd95289e1
AndreClaveria <andreclaveria2002@gmail.com>
2024-02-29 16:50:48 +0100

    ok

--- src/controllers/profil.controller.js
+++ src/controllers/profil.controller.js
@@ -1,39 +1,34 @@
-const db = require('../models'); // Assurez-vous de mettre le chemin correct vers votre fichier de modèles Sequelize
+const db = require("../models"); // Assurez-vous de mettre le chemin correct vers votre fichier de modèles Sequelize
 
 module.exports = {
     getProfil: async (req, res) => {
-
-        console.log(req.user, "token decoded");
-
         try {
+            // Utilisez le modèle Sequelize User pour rechercher l'utilisateur par ID
+            const user = await db.User.findByPk(req.user);
 
-        // Utilisez le modèle Sequelize User pour rechercher l'utilisateur par ID
-        const user = await db.User.findByPk(req.user);
+            // Vérifiez si l'utilisateur a été trouvé
+            if (!user) {
+                return res.status(404).json({
+                    success: false,
+                    message: "User not found",
+                });
+            }
 
-        // Vérifiez si l'utilisateur a été trouvé
-        if (!user) {
-            return res.status(404).json({
-            success: false,
-            message: "User not found",
+            // Retournez les informations de l'utilisateur au format JSON
+            return res.status(200).json({
+                success: true,
+                user: {
+                    id: user.id,
+                    firstname: user.firstname,
+                    lastname: user.lastname,
+                    email: user.email,
+                    address: user.address,
+                    zipcode: user.zipcode,
+                    city: user.city,
+                    phone: user.phone,
+                },
+                message: "User profile retrieved successfully",
             });
-        }
-
-        // Retournez les informations de l'utilisateur au format JSON
-        return res.status(200).json({
-            success: true,
-            user: {
-                id: user.id,
-                firstname: user.firstname,
-                lastname: user.lastname,
-                email: user.email,
-                address: user.address,
-                zipcode: user.zipcode,
-                city: user.city,
-                phone: user.phone,
-            },
-            message: "User profile retrieved successfully",
-        });
-
         } catch (error) {
             console.error(error);
             res.status(500).json({
@@ -41,5 +36,5 @@ module.exports = {
                 message: "Internal Server Error. Something went wrong.",
             });
         }
-    }
+    },
 };
