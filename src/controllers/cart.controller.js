65945c3aa42f4ce66f772ef87fa0264866245e7c
AndreClaveria <andreclaveria2002@gmail.com>
2024-03-21 12:23:15 +0100

    add, get, delete écrit

---
+++ src/controllers/cart.controller.js
@@ -0,0 +1,9 @@
+const db = require("../models"); // Assuming your models are in the '../models' directory
+
+module.exports = {
+    addCart: async (req, res) => {},
+
+    getCart: async (req, res) => {},
+
+    deleteCart: async (req, res) => {},
+};
